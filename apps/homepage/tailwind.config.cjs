b6bafde80adff5ce2f824a44a8f1201902f10f75
Martin Jordán <martin@jordan.cz>
2023-03-06 13:50:20 +0100

    Chore: Add Flowbite

--- apps/homepage/tailwind.config.cjs
+++ apps/homepage/tailwind.config.cjs
@@ -1,7 +1,11 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/flowbite/**/*.js',
+	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [require('flowbite/plugin')],
 };
