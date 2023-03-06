527910b19638079a7cf70fa7c787cdac7b85082f
Martin Jordán <martin@jordan.cz>
2023-03-06 13:51:37 +0100

    New: Add constants

---
+++ apps/homepage/src/constants.ts
@@ -0,0 +1,18 @@
+export const PAGE_TITLE = 'martinjordan.dev';
+export const ID_ABOUT_ME = 'about-me';
+export const ID_HERO = 'hero';
+export const ID_EXPERIENCE = 'experience';
+export const ID_MY_PROJECTS = 'my-projects';
+export const ID_CONTACT = 'contact';
+export const ABOUT_ME = 'About me';
+export const EXPERIENCE = 'Experience';
+export const MY_PROJECTS = 'My projects';
+export const CONTACT = 'Contact';
+export const RESUME = 'Resume';
+
+export const menuItems = [
+	{ id: ID_ABOUT_ME, value: ABOUT_ME },
+	{ id: ID_EXPERIENCE, value: EXPERIENCE },
+	{ id: ID_MY_PROJECTS, value: MY_PROJECTS },
+	{ id: ID_CONTACT, value: CONTACT },
+];
