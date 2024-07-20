3a468d1b601da1ece9948373f77f5dd9c5289c98
Martin Jordán <martin@jordan.cz>
2024-07-20 10:30:02 +0200

    New: Put new resume also into mobile button

--- apps/homepage/src/components/ResumeButton.tsx
+++ apps/homepage/src/components/ResumeButton.tsx
@@ -2,8 +2,8 @@ import { PDFDownloadLink } from '@react-pdf/renderer';
 import { ReactLinkButton } from './ReactLinkButton';
 import { Resume } from './Resume';
 
-export const ResumeButton = () => (
+export const ResumeButton = ({ children }: { children: React.ReactNode }) => (
   <PDFDownloadLink document={<Resume />} fileName="martin_jordan_resume.pdf">
-    {({ url }) => url && <ReactLinkButton href={url}>Resume</ReactLinkButton>}
+    {({ url }) => url && <ReactLinkButton href={url}>{children}</ReactLinkButton>}
   </PDFDownloadLink>
 );
